fad17217bedf269ed3aac837e46b62fca3a5065a
Paarth Arora <paarth.arora21@gmail.com>
2024-03-27 22:34:26 -0400

    Added logic for what happens when you add the DEL button and overwrite capabilities when you add a digit after pressing the = operation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite){
+        return{
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -25,7 +32,21 @@ function reducer(state, { type, payload }) {
         currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
     case ACTIONS.CLEAR:
-      return {}
+      if (state.overwrite){
+        return {
+          ...state,
+          overwrite: false, 
+          currentOperand: null,
+        }
+      }
+      return {
+        ...state,
+        currentOperand: null,
+        previousOperand: null,
+        operation: null
+      }
+
+      
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previousOperand == null) {
         return state
@@ -52,6 +73,24 @@ function reducer(state, { type, payload }) {
         operation: payload.operation,
         currentOperand: null,
       }
+    case ACTIONS.DELETE_DIGIT:
+      if (state.currentOperand == null){
+        return state
+      }
+
+      if (state.currentOperand.length == 1){
+        return {
+          ...state,
+          currentOperand: null
+        }
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+
+      }
+
+    
     case ACTIONS.EVALUATE:
       if (state.currentOperand == null 
         || state.previousOperand == null 
@@ -60,6 +99,7 @@ function reducer(state, { type, payload }) {
         }
       return {
         ...state,
+        overwrite: true,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
@@ -111,7 +151,7 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
 
       <OperationButton operation="÷" dispatch={dispatch} />
 
